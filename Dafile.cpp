f5888860bef7fd87ddcf1dff88b9057e3efa4678
helloworld3773 <zahraa.irfan.sorathia@gmail.com>
2026-05-01 17:03:10 +0400

    Beginning da project
    Making basic commands before starting the gates

--- Dafile.cpp
+++ Dafile.cpp
@@ -3,5 +3,17 @@
 using namespace std;
 
 int main () {
-  
+char x;
+cin >> x;
+  cout << "welcome to this project" << "\n";
+cout << "to quit the execution of code type q and send to the console" << "\n";
+  cout << "to see the other commands type l and send to the console" << "\n";
+  cout << "remember that capitalisation matters" << "\n";
+    cout << "-----------------------" << "\n";
+      if (x == q) {
+      return 0;
+    }
+  else if (x == l) {
+    cout << "X for the x gate" << "\n";
+  }
 }
