af153c864b5443f3bbe96ba254f0de92fb320eb7
helloworld3773 <zahraa.irfan.sorathia@gmail.com>
2026-05-04 14:30:00 +0400

    Circle
    Adding the circle for Bloch sphere

--- Dafile.cpp
+++ Dafile.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 using namespace std;
 
 int main () {
-char x;
-cin >> x;
+  int r = 10
   cout << "welcome to this project" << "\n";
   cout << "to quit the execution of code type q and send to the console" << "\n";
   cout << "to see the other commands type l and send to the console" << "\n";
   cout << "remember that capitalisation matters" << "\n";
   cout << "-----------------------" << "\n";
+  for (int z = -r; z <= r; z++) {
+    for (int a = -r; a <= r; a++) {
+      if (abs(x * x + y * y - r * r) < r) {
+        cout << "*";
+      }
+        else {
+          cout << " ";
+        }
+      cout << "\n";
+    }
+  }
+  string y = |0>
+  char x;
+cin >> x;
       if (x == q) {
       return 0;
     }
@@ -19,6 +33,7 @@ cin >> x;
     cout << "Y for the y gate" << "\n";
   }
   else if (x == X) {
-    
+    if (y == |0>) {
+    }
   }
 }
